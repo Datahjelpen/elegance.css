9c215917af6857e1cbbb9848682ccf29306838c5
Bear Hagen <b@datahjelpen.no>
2018-02-14 02:55:27 +0100

    Setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
-var webpack = require("webpack");
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const webpack = require("webpack");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
-	entry: {
-	},
+	entry: path.resolve(__dirname, 'src')+'/js/app.js',
 	output: {
+		path: path.resolve(__dirname, 'dist/assets'),
+		filename: 'app.js',
+		publicPath: path.resolve(__dirname, 'dist/assets'),
 	},
 	module: {
 		rules: [{
