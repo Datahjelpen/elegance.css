0c52cf2aa1431895773ad24ff1c00f37c78d894c
Bear Hagen <b@datahjelpen.no>
2018-03-21 15:57:26 +0100

    Removed unused code

--- src/js/elegance_css.js
+++ src/js/elegance_css.js
@@ -1,9 +1,5 @@
-// const loadCSS = require('fg-loadcss');
-
 import '../scss/elegance.scss';
 import './navigation.js';
 
 // Let's load all images so they all get compressed and usable
 const images = require.context('./../images', false, /\.(gif|png|jpe?g|svg)$/i);
-
-// loadCSS.loadCSS('/assets/css/elegance.css');
