a1a4893404ec0358b519644c08304d96ec036e20
Bear Hagen <b@datahjelpen.no>
2018-03-26 00:51:01 +0200

    Moved the window.is_mobile/tablet/desktop from nav js to main file

--- src/js/elegance_css.js
+++ src/js/elegance_css.js
@@ -1,6 +1,32 @@
 // Let's load all images so they all get compressed and usable
 const images = require.context('./../images', false, /\.(gif|png|jpe?g|svg)$/i);
 
+import throttle from 'lodash.throttle';
+
+(function() {
+	// Create the devicecheck element
+	var div = document.createElement('div');
+	div.setAttribute('id', 'devicecheck');
+	document.body.appendChild(div);
+
+	// Trigger update on resize
+	window.addEventListener('resize', throttle(window_is_sizeUpdate, 500));
+	window_is_sizeUpdate(); // Init
+
+	// Update variables
+	function window_is_sizeUpdate() {
+		var devicecheck = window.getComputedStyle(document.getElementById("devicecheck")).getPropertyValue("content");
+
+		window.is_desktop = false;
+		window.is_tablet =  false;
+		window.is_mobile =  false;
+
+		if (devicecheck == "\"desktop\"" || devicecheck == "desktop") window.is_desktop = true;
+		if (devicecheck == "\"tablet\""  || devicecheck == "tablet") window.is_tablet =   true;
+		if (devicecheck == "\"mobile\""  || devicecheck == "mobile") window.is_mobile =   true;
+	};
+})();
+
 import '../scss/elegance.scss';
 import './navigation.js';
 
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,34 +1,5 @@
 import throttle from 'lodash.throttle';
 
-(function() {
-	// Create the devicecheck element
-	var div = document.createElement('div');
-	div.setAttribute('id', 'devicecheck');
-	document.body.appendChild(div);
-
-	// Trigger update on resize
-	var delay;
-	window.addEventListener('resize', function() {
-		clearTimeout(delay);
-		delay = setTimeout(window_is_sizeUpdate, 500);
-	});
-
-	window_is_sizeUpdate(); // Init
-
-	// Update variables
-	function window_is_sizeUpdate() {
-		var devicecheck = window.getComputedStyle(document.getElementById("devicecheck")).getPropertyValue("content");
-
-		window.is_desktop = false;
-		window.is_tablet =  false;
-		window.is_mobile =  false;
-
-		if (devicecheck == "\"desktop\"" || devicecheck == "desktop") window.is_desktop = true;
-		if (devicecheck == "\"tablet\""  || devicecheck == "tablet") window.is_tablet =   true;
-		if (devicecheck == "\"mobile\""  || devicecheck == "mobile") window.is_mobile =   true;
-	};
-})();
-
 // Open and close adaptive/responsive navigations
 (function() {
 	var navigations = [];
